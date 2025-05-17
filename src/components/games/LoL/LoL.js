56ad576407a87ecb579eb79b278c7d0964d5a958
Bauzi <volodiasyd@gmail.com>
2025-05-17 19:15:23 +0300

    removed unused useEffect and Modal

--- src/components/games/LoL/LoL.js
+++ src/components/games/LoL/LoL.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Header from '../../Header';
 import MainName from '../MainName';
 import Nav from '../Nav';
-import Modal from '../Modal';
 
 var path = window.location.pathname;
 var page = path.split('/').pop();
